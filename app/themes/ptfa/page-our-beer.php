37bc4e5820b63caa3f615b356d0a4e6ed43c004d
Isaac Bueno <isaacmabueno@gmail.com>
2017-01-23 02:02:33 -0700

    adding backgrounds to each label

--- app/themes/ptfa/page-our-beer.php
+++ app/themes/ptfa/page-our-beer.php
@@ -3,19 +3,103 @@
   <h1 class="brewline">BEER</h1>
   <hr>
 </section>
-<section class="english-bg">
-  <div class="container-fluid">
-    <div class="row">
-<div class="offset-md-3"></div>
-  <div class="col-xs-12 col-md-3 beer-label">
-    <img src="/isaac/web/app/uploads/2017/01/mild.png" alt="">
-  </div>
 
-  <div class="col-xs-12 col-md-3 beer-desc">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </p>
+<section class="english-bg container-fluid" style="background-image:url('/isaac/web/app/uploads/2017/01/englishbg.png');">
+  <div class="row">
+    <div class="offset-md-2"></div>
+    <div class="col-xs-12 col-md-4 beer-label">
+      <img src="/isaac/web/app/uploads/2017/01/mild.png" alt="">
+    </div>
+    <div class="col-xs-12 col-md-4 beer-desc">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p class="details">
+         ABV: 3.4%<br>
+         FG: 1.009<br>
+         IBU's: 20
+      </p>
+    </div>
+    <div class="offset-md-2"></div>
   </div>
-  <div class="offset-md-3"></div>
+</section>
+
+<section class="esb-bg container-fluid" style="background-image:url('/isaac/web/app/uploads/2017/01/esbbg2.png');">
+  <div class="row">
+    <div class="offset-md-2"></div>
+    <div class="col-xs-12 col-md-4 beer-label">
+      <img src="/isaac/web/app/uploads/2017/01/esb.png" alt="">
+    </div>
+    <div class="col-xs-12 col-md-4 beer-desc">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p class="details">
+         ABV: 3.4%<br>
+         FG: 1.009<br>
+         IBU's: 20
+      </p>
+    </div>
+    <div class="offset-md-2"></div>
+  </div>
+</section>
+
+<section class="stout-bg container-fluid" style="background-image:url('/isaac/web/app/uploads/2017/01/stoutbg.png');">
+  <div class="row">
+    <div class="offset-md-2"></div>
+    <div class="col-xs-12 col-md-4 beer-label">
+      <img src="/isaac/web/app/uploads/2017/01/stout.png" alt="">
+    </div>
+    <div class="col-xs-12 col-md-4 beer-desc">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p class="details">
+         ABV: 3.4%<br>
+         FG: 1.009<br>
+         IBU's: 20
+      </p>
+    </div>
+    <div class="offset-md-2"></div>
+  </div>
+</section>
+
+<section class="ipa-bg container-fluid" style="background-image: url('/isaac/web/app/uploads/2017/01/ipabg.png');">
+  <div class="row">
+    <div class="offset-md-2"></div>
+    <div class="col-xs-12 col-md-4 beer-label">
+      <img src="/isaac/web/app/uploads/2017/01/IPA.png" alt="">
+    </div>
+    <div class="col-xs-12 col-md-4 beer-desc">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p class="details">
+         ABV: 3.4%<br>
+         FG: 1.009<br>
+         IBU's: 20
+      </p>
+    </div>
+    <div class="offset-md-2"></div>
+  </div>
+</section>
+
+<section class="ypa-bg container-fluid" style="background-image: url('/isaac/web/app/uploads/2017/01/bluebeer2.png')";>
+  <div class="row">
+    <div class="offset-md-2"></div>
+    <div class="col-xs-12 col-md-4 beer-label">
+      <img src="/isaac/web/app/uploads/2017/01/yorkshire.png" alt="">
+    </div>
+    <div class="col-xs-12 col-md-4 beer-desc">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <p class="details">
+         ABV: 3.4%<br>
+         FG: 1.009<br>
+         IBU's: 20
+      </p>
+    </div>
+    <div class="offset-md-2"></div>
   </div>
 </section>
