6cfe2e34d57fdd05d5e17817f7032c558728d08e
Isaac Bueno <isaacmabueno@gmail.com>
2017-01-23 00:00:10 -0700

    working on our beer layout, adding color

--- app/themes/ptfa/page-our-beer.php
+++ app/themes/ptfa/page-our-beer.php
@@ -3,3 +3,19 @@
   <h1 class="brewline">BEER</h1>
   <hr>
 </section>
+<section class="english-bg">
+  <div class="container-fluid">
+    <div class="row">
+<div class="offset-md-3"></div>
+  <div class="col-xs-12 col-md-3 beer-label">
+    <img src="/isaac/web/app/uploads/2017/01/mild.png" alt="">
+  </div>
+
+  <div class="col-xs-12 col-md-3 beer-desc">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </p>
+  </div>
+  <div class="offset-md-3"></div>
+  </div>
+</section>
