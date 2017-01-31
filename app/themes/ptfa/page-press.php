ac6297b6c57160331840e832c84a8071dfb1da88
Isaac Bueno <isaacmabueno@gmail.com>
2017-01-30 23:58:24 -0700

    starting point/basic layout with press page. Needs to be styled

--- app/themes/ptfa/page-press.php
+++ app/themes/ptfa/page-press.php
@@ -1,1 +1,43 @@
 <h1>we are in the press, check it out!</h1>
+
+<div>
+  <h1 class="seasonal">PRESS</h1>
+  <p>
+    <center class="details">WHAT PEOPLE ARE SAYING ABOUT OUR BEER</center>
+  </p>
+  <center><span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></center>
+</div>
+<div class="row">
+  <div class="pad col-md-6">
+    <div class="test">
+      <h3><a class="press-link" href="http://barclayperkins.blogspot.com/2016/09/chicago-day-one.html" target=_"blank"><center>Barclay Perkins</center></a></h3>
+      <p class="press-desc"><strong>The lads from Present Tense have a cask of ESB that "needs finishing".</strong> Sounds like a challenge to me. We head for their brewery, which rather
+        exotically is located in a garage in a friend's back yard... The beer is spot on and soon thumping several spots very hard.</p>
+    </div>
+  </div>
+  <div class="pad col-md-6">
+    <div class="test">
+      <h3><a class="press-link" href="http://goodbeerhunting.com/blog/2016/3/14/getting-real-chicagos-present-tense-fine-ales" target="_blank"><center>Good Beer Hunting</center></a></h3>
+        <p class="press-desc"><strong>Getting Real — Chicago’s Present Tense Fine Ales.</strong> Ten years ago, the idea of a brewery focusing strictly on English Cask Ales wouldn’t have
+        seemed like a stretch. But with today’s reliance on “extreme” brewing techniques—aggressive hopping, barrel-aging, yeast sourced from beards, etc.—that
+        generate hype, there’s a niche to be filled, as absurd as it may sound.</p>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="pad col-md-6">
+    <div class="test">
+      <h3><a class="press-link" href="https://www.kegworks.com/blog/cask-ale-in-the-united-states/" target="_blank"><center>KegWorks</center></a></h3>
+        <p class="press-desc"><strong>The Pleasures (and Occasional Pitfalls) of Cask Ale in the United States.</strong> Tyler Jackson and Thomas Thorpe know all too well the challenges
+        cask ale presents. They are co-owners of Chicago’s Present Tense Fine Ales, a cask ale brewery-in-planning that aims to be known for remarkable, traditional
+        English beer.</p>
+    </div>
+  </div>
+  <div class="pad col-md-6">
+    <div class="test">
+      <h3><a class="press-link" href="http://boakandbailey.com/2015/11/news-nuggets-longreads-14112015/" target="_blank"><center>Boake and Bailey</center></a></h3>
+        <p class="press-desc"><strong>An American brewer makes his first long-anticipated visit to an English pub (via @RoosterOl):</strong> The right door…the left door – the ultimate
+        decision. There is certainly a good explanation for the two separate doors; however, I have yet to hear it.</p>
+    </div>
+  </div>
+</div>
