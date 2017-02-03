f5c65f75316a6d958c5b2c221eb8baffad93f746
Isaac Bueno <isaacmabueno@gmail.com>
2017-02-02 18:29:44 -0700

    updating our beers page and making responsive

--- app/themes/ptfa/page-our-beer.php
+++ app/themes/ptfa/page-our-beer.php
@@ -6,12 +6,12 @@
 
 <section class="english-bg container-fluid" style="background-image:url('/isaac/web/app/uploads/2017/01/englishbg.png');">
   <div class="row">
-    <div class="offset-md-2"></div>
-    <div class="col-xs-12 col-md-4 beer-label">
+    <div class="offset-md-1"></div>
+    <div class="col-xs-10 offset-xs-1 col-md-5 beer-label">
       <img src="/isaac/web/app/uploads/2017/01/mild.png" alt="">
     </div>
-    <div class="col-xs-12 col-md-4 beer-desc">
-      <p>
+    <div class="col-xs-10 offset-xs-1 col-md-5 beer-desc">
+      <p class="label-desc">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
       <p class="details">
@@ -20,18 +20,18 @@
          IBU's: 20
       </p>
     </div>
-    <div class="offset-md-2"></div>
+    <div class="offset-md-1"></div>
   </div>
 </section>
 
 <section class="esb-bg container-fluid" style="background-image:url('/isaac/web/app/uploads/2017/01/esbbg2.png');">
   <div class="row">
-    <div class="offset-md-2"></div>
-    <div class="col-xs-12 col-md-4 beer-label">
+    <div class="offset-md-1"></div>
+    <div class="col-xs-10 offset-xs-1 col-md-5 beer-label">
       <img src="/isaac/web/app/uploads/2017/01/esb.png" alt="">
     </div>
-    <div class="col-xs-12 col-md-4 beer-desc">
-      <p>
+    <div class="col-xs-10 offset-xs-1 col-md-5 beer-desc">
+      <p class="label-desc">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
       <p class="details">
@@ -40,18 +40,18 @@
          IBU's: 20
       </p>
     </div>
-    <div class="offset-md-2"></div>
+    <div class="offset-md-1"></div>
   </div>
 </section>
 
 <section class="stout-bg container-fluid" style="background-image:url('/isaac/web/app/uploads/2017/01/stoutbg.png');">
   <div class="row">
-    <div class="offset-md-2"></div>
-    <div class="col-xs-12 col-md-4 beer-label">
+    <div class="offset-md-1"></div>
+    <div class="col-xs-10 offset-xs-1 col-md-5 beer-label">
       <img src="/isaac/web/app/uploads/2017/01/stout.png" alt="">
     </div>
-    <div class="col-xs-12 col-md-4 beer-desc">
-      <p>
+    <div class="col-xs-10 offset-xs-1 col-md-5 beer-desc">
+      <p class="label-desc">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
       </p>
       <p class="details">
@@ -60,39 +60,39 @@
          IBU's: 20
       </p>
     </div>
-    <div class="offset-md-2"></div>
+    <div class="offset-md-1"></div>
   </div>
 </section>
 
 <section class="ipa-bg container-fluid" style="background-image: url('/isaac/web/app/uploads/2017/01/ipabg.png');">
   <div class="row">
-    <div class="offset-md-2"></div>
-    <div class="col-xs-12 col-md-4 beer-label">
+    <div class="col-md-1"></div>
+    <div class="col-xs-10 offset-xs-1 col-md-5 beer-label">
       <img src="/isaac/web/app/uploads/2017/01/IPA.png" alt="">
     </div>
-    <div class="col-xs-12 col-md-4 beer-desc">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <div class="col-xs-12 col-md-5 beer-desc">
+      <p class="label-desc">
+        For all the bandwagon hop heads and fickle, uninformed craft beer dabblers... this beer is not about following the crowd or joining the masses of taste bud destroying, one and done beers…this is a different type of IPA; the unassuming, yet assertive type; the type that need not outdo another because this beer simply is what it is – upfront hoppy, floral character complimenting a grainy, classic English malt profile
       </p>
       <p class="details">
-         ABV: 3.4%<br>
-         FG: 1.009<br>
-         IBU's: 20
+         ABV: 5.6%<br>
+         FG: 1.011<br>
+         IBU's: 46
       </p>
     </div>
-    <div class="offset-md-2"></div>
+    <div class="col-md-1"></div>
   </div>
 </section>
 
-<section class="ypa-bg container-fluid" style="background-image: url('/isaac/web/app/uploads/2017/01/bluebeer2.png')";>
+<section class="ypa-bg container-fluid" style="background-image: url('/isaac/web/app/uploads/2017/02/yorkshirepaleale30.png')";>
   <div class="row">
-    <div class="offset-md-2"></div>
-    <div class="col-xs-12 col-md-4 beer-label">
+    <div class="col-md-1"></div>
+    <div class="col-xs-10 offset-xs-1 col-md-5 beer-label">
       <img src="/isaac/web/app/uploads/2017/01/yorkshire.png" alt="">
     </div>
-    <div class="col-xs-12 col-md-4 beer-desc">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    <div class="col-xs-10 offset-xs-1 col-md-5 beer-desc">
+      <p class="label-desc">
+        Light, crisp and easy drinking - the perfect beer for having another and maybe another. A soft water profile creates a pleasing context for showcasing true English hops and Maris Otter malt. Simply balanced.
       </p>
       <p class="details">
          ABV: 3.4%<br>
@@ -100,6 +100,6 @@
          IBU's: 20
       </p>
     </div>
-    <div class="offset-md-2"></div>
+    <div class="col-md-1"></div>
   </div>
 </section>
